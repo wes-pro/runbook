ebbcf6e8b528dc1d86feb283cc18374b11ac6fd4
Michal Wesolowski <michal@wespro.pl>
2024-11-21 14:30:13 +0100

    New buttons added

--- runbook.py
+++ runbook.py
@@ -26,7 +26,7 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=loglevel)
 st.set_page_config(page_title="RunBook", layout="centered", initial_sidebar_state="collapsed", menu_items={})
 
 
-@st.cache_resource
+#@st.cache_resource
 def load_command_files():
     cmd_series = {}
     for filename in args.config_file:
@@ -59,25 +59,13 @@ def check_password():
 
 
 def zellij_run_command(cmd):
-    cmd = cmd.replace('$', '\\$').strip().splitlines()
+    cmd = cmd.strip().splitlines()
     for i, line in enumerate(cmd):
-        if i < len(cmd) - 1:
-            zcmd = f'zellij --session "{args.session_name}" action write-chars \"{line}\n\"'
-        else:
-            if exec_button:
-                zcmd = f'zellij --session "{args.session_name}" action write-chars \"{line}\n\"'
-            else:
-                zcmd = f'zellij --session "{args.session_name}" action write-chars \"{line}\"'
+        line_bytes = ' '.join([str(b) for b in list(line.encode())])
+        if exec_button or i < len(cmd) - 1:
+            line_bytes += ' 10'
+        zcmd = f'zellij --session "{args.session_name}" action write {line_bytes}'
         os.system(zcmd)
-
-    #if exec_button:
-    #    cmd += "\n"
-    #zcmd = f'zellij --session "{args.session_name}" action write-chars \"\n\"'
-    #os.system(zcmd)
-    # zcmd = f'zellij --session "{args.session_name}" action write-chars \"{cmd}\"'
-    # os.system(zcmd)
-
-
 
 
 def zellij_prev_tab():
@@ -122,10 +110,16 @@ def zellij_scroll_down():
         os.system(zcmd)
 
 
+def zellij_ctrl_c():
+    zcmd = f'zellij --session "{args.session_name}" action write-chars \"\x03\n\"'
+    os.system(zcmd)
+
+
 def zellij_clear_screen():
     if st.session_state.curr_command['lang'] == 'sql':
-        zcmd = f'zellij --session "{args.session_name}" action write-chars \"\x03\n\"'
-        os.system(zcmd)
+        zellij_ctrl_c()
+        #zcmd = f'zellij --session "{args.session_name}" action write-chars \"\x03\n\"'
+        #os.system(zcmd)
         cmd = "clear screen\n"
     else:
         cmd = "clear\n"
@@ -140,7 +134,7 @@ def zellij_enter():
 
 def select_next_cmd():
     curr_cmd_index = st.session_state.curr_series['commands'].index(st.session_state.curr_command)
-    if exec_button:
+    if exec_button or insert_on_move:
         zellij_run_command(st.session_state.curr_command['cmd'])
     if curr_cmd_index < len(st.session_state.curr_series['commands']) - 1:
         st.session_state.curr_command = st.session_state.curr_series['commands'][curr_cmd_index+1]
@@ -158,10 +152,10 @@ def update_selection(curr_series, curr_command, execute=False, undo=False):
     if execute:
         if args.session_name:
             if undo:
-                if 'rcmd' in curr_command:
-                    logger.debug(f"About to execute below Zellij command into session {args.session_name}:\n{curr_command['rcmd']}")
-                    pyperclip.copy(curr_command['rcmd'])
-                    zellij_run_command(curr_command['rcmd'])
+                if 'undo' in curr_command:
+                    logger.debug(f"About to execute below Zellij command into session {args.session_name}:\n{curr_command['undo']}")
+                    pyperclip.copy(curr_command['undo'])
+                    zellij_run_command(curr_command['undo'])
             else:
                 logger.debug(f"About to execute below Zellij command into session {args.session_name}:\n{curr_command['cmd']}")
                 pyperclip.copy(curr_command['cmd'])
@@ -174,7 +168,7 @@ def render_command_table(cmd_series):
     curr_cmd_index = st.session_state.curr_series['commands'].index(st.session_state.curr_command)
     if cmd_series['name'] not in st.session_state.sel_helpers:
         st.session_state.sel_helpers[cmd_series['name']] = []
-    with st.container(height=300):
+    with st.container(height=330):
         for i, cmd in enumerate(cmd_series['commands']):
             if len(search) >= 3 and search.lower() not in cmd['name'].lower():
                 continue
@@ -190,7 +184,7 @@ def render_command_table(cmd_series):
             with col2:
                 st.button('', icon=':material/directions_run:', key=f'{cmd_series['name']}-exec-{i}', on_click=update_selection, args=(cmd_series, cmd, True))
             with col3:
-                if 'rcmd' in cmd:
+                if 'undo' in cmd:
                     st.button('', icon=':material/undo:', key=f'{cmd_series['name']}-undo-{i}', on_click=update_selection, args=(cmd_series, cmd, True, True))
 
 
@@ -226,45 +220,58 @@ with st.container(key='remote'):
     col_r11, col_r12, col_r13, col_r14 = st.columns([1,1,1,1], vertical_alignment='center')
 
     with col_r11:
-        with st.container(key='col_exec'):
-            exec_button = st.checkbox('AutoExec')
-            #st.button('clear_screen', icon=':material/mop:', use_container_width=True, key='clear_screen', on_click=zellij_clear_screen)
+        exec_button = st.checkbox('AutoExec')
     with col_r12:
         st.button('', icon=':material/arrow_circle_left:', key='tab_left', use_container_width=True, on_click=zellij_prev_tab)
-        st.button('', icon=':material/keyboard_double_arrow_left:', key='pane_left', use_container_width=True, on_click=zellij_prev_pane)
     with col_r13:
         st.button('', icon=':material/arrow_circle_right:', key='tab_right', use_container_width=True, on_click=zellij_next_tab)
-        st.button('', icon=':material/keyboard_double_arrow_right:', key='pane_right', use_container_width=True, on_click=zellij_next_pane)
     with col_r14:
         if exec_button:
             st.button('select_right', icon=':material/start:', key='select_right', type='primary', use_container_width=True, on_click=select_next_cmd)
-            st.button('select_enter', icon=':material/keyboard_return:', key='select_enter', type='secondary', use_container_width=True, on_click=zellij_enter)
         else:
             st.button('select_right', icon=':material/start:', key='select_right', use_container_width=True, on_click=select_next_cmd)
-            st.button('select_enter', icon=':material/keyboard_return:', key='select_enter', type='primary', use_container_width=True, on_click=zellij_enter)
-        #st.button('select_enter', icon=':material/keyboard_return:', key='select_enter', type='primary', use_container_width=True, on_click=zellij_enter)
 
     col_r21, col_r22, col_r23, col_r24 = st.columns([1,1,1,1], vertical_alignment='center')
     with col_r21:
-        st.button('', icon=':material/fullscreen:', key='fullscreen', use_container_width=True, on_click=zellij_toogle_fullscreen)
+        insert_on_move = st.checkbox('AutoInsert')
     with col_r22:
-        st.button('', icon=':material/sync:', key='toogle_tab_sync', use_container_width=True, on_click=zellij_toggle_sync_tab)
+        st.button('', icon=':material/keyboard_double_arrow_left:', key='pane_left', use_container_width=True, on_click=zellij_prev_pane)
+    with col_r23:
+        st.button('', icon=':material/keyboard_double_arrow_right:', key='pane_right', use_container_width=True, on_click=zellij_next_pane)
+    with col_r24:
+        if exec_button:
+            st.button('select_enter', icon=':material/keyboard_return:', key='select_enter', type='secondary', use_container_width=True, on_click=zellij_enter)
+        else:
+            st.button('select_enter', icon=':material/keyboard_return:', key='select_enter', type='primary', use_container_width=True, on_click=zellij_enter)
+
+    '---'
 
     col_r31, col_r32, col_r33, col_r34 = st.columns([1,1,1,1], vertical_alignment='center')
     with col_r31:
-        st.button('clear_screen', icon=':material/mop:', use_container_width=True, key='clear_screen', on_click=zellij_clear_screen)
+        pass
     with col_r32:
         st.button('', icon=':material/more_down:', key='more_down', use_container_width=True, on_click=zellij_scroll_down)
     with col_r33:
         st.button('', icon=':material/more_up:', key='more_up', use_container_width=True, on_click=zellij_scroll_up)
     with col_r34:
+        with st.container(key='cancel'):
+            st.button('ctrl_c', icon=':material/cancel:', key='ctrl_c', use_container_width=True, on_click=zellij_ctrl_c)
+
+    col_r41, col_r42, col_r43, col_r44 = st.columns([1,1,1,1], vertical_alignment='center')
+    with col_r41:
+        st.button('', icon=':material/fullscreen:', key='fullscreen', use_container_width=True, on_click=zellij_toogle_fullscreen)
+    with col_r42:
+        st.button('', icon=':material/mop:', use_container_width=True, key='clear_screen', on_click=zellij_clear_screen)
+    with col_r43:
+        st.button('', icon=':material/sync:', key='toogle_tab_sync', use_container_width=True, on_click=zellij_toggle_sync_tab)
+    with col_r44:
         st.button('select_left', icon=':material/keyboard_tab_rtl:', key='select_left', use_container_width=True, on_click=select_prev_cmd)
 
 
 '---'
 
-with st.container(key='cmd_content', height=300):
+with st.container(key='cmd_content', height=400):
     st.code(st.session_state.curr_command['cmd'], language=st.session_state.curr_command['lang'])
 
 #add_keyboard_shortcuts({"ArrowRight": "select_right", "ArrowLeft": "select_left"})
-add_keyboard_shortcuts({"PageDown": "select_right", "PageUp": "select_left"})
+add_keyboard_shortcuts({"PageDown": "select_right", "PageUp": "select_left", "Enter": "select_enter", "Home": "ctrl_c"})
